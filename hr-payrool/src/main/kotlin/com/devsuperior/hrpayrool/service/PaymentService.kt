16b2927e55f658989676da94a1e68009ac26cf16
nelsonsozinho <nelsonsozinho@gmail.com>
2021-03-27 16:20:23 -0400

    integration via uri

--- hr-payrool/src/main/kotlin/com/devsuperior/hrpayrool/service/PaymentService.kt
+++ hr-payrool/src/main/kotlin/com/devsuperior/hrpayrool/service/PaymentService.kt
@@ -1,13 +1,26 @@
 package com.devsuperior.hrpayrool.service
 
 import com.devsuperior.hrpayrool.model.Payment
+import com.devsuperior.hrpayrool.model.Worker
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import java.util.*
 
 @Service
-class PaymentService {
+class PaymentService(private val restTemplate: RestTemplate) {
+
+    @Value("\${hr-worker.host}")
+    lateinit var workerHost: String
 
     fun getPayment(worker: Long, days: Int): Payment {
-        return Payment("Bob", 200.0, days)
+        val uriVariables: HashMap<String, String> = HashMap()
+        uriVariables.put("id", worker.toString())
+
+        val worker: Worker? = restTemplate.getForObject("$workerHost/worker/{id}", Worker::class.java, uriVariables)
+
+        return Payment(worker!!.name, worker!!.dailyIncome, days)
     }
 
 }
