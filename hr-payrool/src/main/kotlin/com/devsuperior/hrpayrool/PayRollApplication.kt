9c3986bc025e1f2bc808503b21432ec7f0abf3a6
nelsonsozinho <nelsonsozinho@gmail.com>
2021-04-01 23:27:50 -0400

    load balance ribbon

--- hr-payrool/src/main/kotlin/com/devsuperior/hrpayrool/PayRollApplication.kt
+++ hr-payrool/src/main/kotlin/com/devsuperior/hrpayrool/PayRollApplication.kt
@@ -2,10 +2,12 @@ package com.devsuperior.hrpayrool
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.ribbon.RibbonClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @EnableFeignClients
 @SpringBootApplication
+@RibbonClient("hr-worker")
 class PayRollApplication
 
 fun main(args: Array<String>) {
--- hr-worker/src/main/kotlin/com/devsuperior/hrpayrool/controller/WorkerController.kt
+++ hr-worker/src/main/kotlin/com/devsuperior/hrpayrool/controller/WorkerController.kt
@@ -2,6 +2,9 @@ package com.devsuperior.hrpayrool.controller
 
 import com.devsuperior.hrpayrool.model.Worker
 import com.devsuperior.hrpayrool.service.WorkerService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.core.env.Environment
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,13 +13,20 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/worker")
-class WorkerController(private val workerService: WorkerService) {
+class WorkerController(
+        private val workerService: WorkerService,
+        private val env: Environment) {
+
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
 
     @GetMapping
     fun listAllWorkers(): ResponseEntity<List<Worker>> = ResponseEntity.ok(workerService.listAllWorkers());
 
     @GetMapping("/{id}")
-    fun getWorker(@PathVariable("id") id: Long): ResponseEntity<Worker> = ResponseEntity.ok(workerService.findWorker(id));
+    fun getWorker(@PathVariable("id") id: Long): ResponseEntity<Worker> {
+        logger.info("PORT = " + env.getProperty("local.server.port"))
+        return ResponseEntity.ok(workerService.findWorker(id))
+    };
 
 }
