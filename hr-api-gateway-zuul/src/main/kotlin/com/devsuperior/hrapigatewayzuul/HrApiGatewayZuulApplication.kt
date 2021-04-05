2c96423db4d3a72c1725518f6cb43a1e2dca5dbe
nelsonsozinho <nelsonsozinho@gmail.com>
2021-04-05 12:01:01 -0400

    new project: api gateway

---
+++ hr-api-gateway-zuul/src/main/kotlin/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.kt
@@ -0,0 +1,11 @@
+package com.devsuperior.hrapigatewayzuul
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrApiGatewayZuulApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrApiGatewayZuulApplication>(*args)
+}
