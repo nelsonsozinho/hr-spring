d455b0a9102b2436308860a0c49e3c0bc32426a8
nelsonsozinho <nelsonsozinho@gmail.com>
2021-04-05 13:25:55 -0400

    gateway works

--- hr-api-gateway-zuul/src/main/kotlin/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.kt
+++ hr-api-gateway-zuul/src/main/kotlin/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.kt
@@ -2,7 +2,11 @@ package com.devsuperior.hrapigatewayzuul
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 class HrApiGatewayZuulApplication
 
