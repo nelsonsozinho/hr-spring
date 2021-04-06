cada11f70a148cac24b04d52a6c921bfd2b9764e
nelsonsozinho <nelsonsozinho@gmail.com>
2021-04-05 21:07:07 -0400

    actuator for runtime config refresh

--- hr-worker/src/main/kotlin/com/devsuperior/hreurekaserver/controller/WorkerController.kt
+++ hr-worker/src/main/kotlin/com/devsuperior/hreurekaserver/controller/WorkerController.kt
@@ -5,6 +5,7 @@ import com.devsuperior.hreurekaserver.service.WorkerService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.core.env.Environment
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.ws.rs.core.Response
 
+@RefreshScope
 @RestController
 @RequestMapping("/worker")
 class WorkerController(
