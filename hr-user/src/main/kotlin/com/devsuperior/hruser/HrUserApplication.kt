58ca970c139d099e1d926c4c3ee6e1c462e66eb6
nelsonsozinho <nelsonsozinho@gmail.com>
2021-04-06 13:23:20 -0400

    hr-user project

---
+++ hr-user/src/main/kotlin/com/devsuperior/hruser/HrUserApplication.kt
@@ -0,0 +1,11 @@
+package com.devsuperior.hruser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrUserApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrUserApplication>(*args)
+}
