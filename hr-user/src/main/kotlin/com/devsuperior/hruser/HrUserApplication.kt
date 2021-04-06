ef94ec2f554c43f5e6996b1a3bd398f5b1772b90
nelsonsozinho <nelsonsozinho@gmail.com>
2021-04-06 13:25:07 -0400

    configure hr-user project

--- hr-user/src/main/kotlin/com/devsuperior/hruser/HrUserApplication.kt
+++ hr-user/src/main/kotlin/com/devsuperior/hruser/HrUserApplication.kt
@@ -2,7 +2,9 @@ package com.devsuperior.hruser
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
 class HrUserApplication
 
