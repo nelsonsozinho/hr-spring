27943cf669435efbe09ec96da5668b1cfb9a10a9
nelsonsozinho <nelsonsozinho@gmail.com>
2021-04-05 20:59:13 -0400

    hr-work as config server client

--- hr-worker/build.gradle.kts
+++ hr-worker/build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.cloud:spring-cloud-config-client")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.h2database:h2")
--- hr-worker/src/main/kotlin/com/devsuperior/hreurekaserver/controller/WorkerController.kt
+++ hr-worker/src/main/kotlin/com/devsuperior/hreurekaserver/controller/WorkerController.kt
@@ -4,12 +4,14 @@ import com.devsuperior.hreurekaserver.model.Worker
 import com.devsuperior.hreurekaserver.service.WorkerService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.env.Environment
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.ws.rs.core.Response
 
 @RestController
 @RequestMapping("/worker")
@@ -19,6 +21,8 @@ class WorkerController(
 
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
+    @Value("\${test.config}")
+    lateinit var testConfig: String
 
     @GetMapping
     fun listAllWorkers(): ResponseEntity<List<Worker>> = ResponseEntity.ok(workerService.listAllWorkers());
@@ -28,5 +32,11 @@ class WorkerController(
         logger.info("PORT = " + env.getProperty("local.server.port"))
         return ResponseEntity.ok(workerService.findWorker(id))
     };
+
+    @GetMapping("/configs")
+    fun getConfigs(): ResponseEntity<Worker> {
+        logger.info("CONFIG = $testConfig")
+        return ResponseEntity.noContent().build()
+    }
 
 }
