f4a57adad5cfda93a787293823f6947683ac8a79
nelsonsozinho <nelsonsozinho@gmail.com>
2021-04-08 17:16:21 -0400

    configure hr-oauth

--- hr-oauth/src/main/kotlin/com/devsuperior/hroauth/HrOauthApplication.kt
+++ hr-oauth/src/main/kotlin/com/devsuperior/hroauth/HrOauthApplication.kt
@@ -3,7 +3,9 @@ package com.devsuperior.hroauth
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
 class HrOauthApplication
 
