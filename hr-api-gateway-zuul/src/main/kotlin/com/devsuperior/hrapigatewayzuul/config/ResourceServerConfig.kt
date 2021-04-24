508e5a3d54b48b05d8e5a248609a139fa119059b
nelsonsozinho <nelsonsozinho@gmail.com>
2021-04-24 18:09:26 -0400

    cors configuration

--- hr-api-gateway-zuul/src/main/kotlin/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.kt
+++ hr-api-gateway-zuul/src/main/kotlin/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.kt
@@ -1,13 +1,21 @@
 package com.devsuperior.hrapigatewayzuul.config
 
+
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 @Configuration
 @EnableResourceServer
@@ -30,6 +38,29 @@ class ResourceServerConfig: ResourceServerConfigurerAdapter() {
                 "/hr-worker/actuator/**",
                 "/hr-oauth/actuator/**").hasRole("ADMIN")
             .anyRequest().authenticated()
+
+        http.cors().configurationSource(corsConfigurationSource())
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val corsConfig: CorsConfiguration = CorsConfiguration()
+        corsConfig.allowedOrigins = mutableListOf("*")
+        corsConfig.allowedMethods = mutableListOf("POST", "GET", "PUT", "DELETE", "PATCH")
+        corsConfig.allowCredentials = true
+        corsConfig.allowedHeaders = mutableListOf("Authorization", "Content-Type")
+
+        val source: UrlBasedCorsConfigurationSource = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+
+        return source
+    }
+
+    @Bean
+    fun corsFilter(): FilterRegistrationBean<CorsFilter> {
+        val bean: FilterRegistrationBean<CorsFilter> = FilterRegistrationBean(CorsFilter(corsConfigurationSource()))
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
     }
 }
 
